039f0fe3a8d686675498736e67d13c9635f332e2
huyquang.do <huydo241996@gmail.com>
2022-05-02 09:07:32 +0700

    feat: add routing

---
+++ src/router/index.ts
@@ -0,0 +1,9 @@
+import {createRouter, createWebHistory} from "vue-router";
+import {routes} from "./routes";
+
+const history = createWebHistory()
+
+export const router = createRouter({
+	history,
+	routes
+})
